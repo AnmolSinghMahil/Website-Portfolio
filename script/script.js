a901e3d6b89ed50ba5cb042cc3ed03a714daf441
AnmolSinghMahil <63561078+AnmolSinghMahil@users.noreply.github.com>
2021-09-24 19:39:20 -0700

    event delegation
    improved scrolling to sections

--- script/script.js
+++ script/script.js
@@ -13,34 +13,51 @@ closeModal.addEventListener('click',function(){
 })
 
 
-const homeScroll = document.querySelector('#homelink');
-const homeSection = document.querySelector('#home-section');
-
-homeScroll.addEventListener('click',function(e){
-    e.preventDefault();
-    homeSection.scrollIntoView({behavior:"smooth"});
-
-})
 
 
-const aboutScroll = document.getElementById('aboutlink');
-const aboutSection = document.querySelector('#about-section');
-aboutScroll.addEventListener('click',function(e){
-    e.preventDefault();
-    aboutSection.scrollIntoView({behavior:"smooth"});
-    console.log('about was clicked');
-})
+// const homeScroll = document.querySelector('#homelink');
+// const homeSection = document.querySelector('#home-section');
+
+// homeScroll.addEventListener('click',function(e){
+//     e.preventDefault();
+//     homeSection.scrollIntoView({behavior:"smooth"});
+
+// })
 
 
-const portfolioSection = document.querySelector('#portfolio-section');
-const portfolioScroll = document.querySelector('#Portfolio');
+// const aboutScroll = document.getElementById('aboutlink');
+// const aboutSection = document.querySelector('#about-section');
+// aboutScroll.addEventListener('click',function(e){
+//     e.preventDefault();
+//     aboutSection.scrollIntoView({behavior:"smooth"});
+//     console.log('about was clicked');
+// })
 
-portfolioScroll.addEventListener('click',function(e){
-    e.preventDefault();
-    portfolioSection.scrollIntoView({behavior:"smooth"});
-})
+
+// const portfolioSection = document.querySelector('#portfolio-section');
+// const portfolioScroll = document.querySelector('#Portfolio');
+
+// portfolioScroll.addEventListener('click',function(e){
+//     e.preventDefault();
+//     portfolioSection.scrollIntoView({behavior:"smooth"});
+// })
 
  
+// Event delegation 
+// select the commmon element which is '.list'
+// determine where the target event is happening with e.target
+document.querySelector('.list').addEventListener('click', function (e) {
+
+    e.preventDefault();
+    //we only want to select the target with class 'scroll' not something around the link
+    if (e.target.classList.contains('scroll')) {
+    // get the href attribute which has the section you want to go to
+        const id = e.target.getAttribute('href');
+    // and select e.target/id and use the function scrollIntoView on that id, like section--2
+        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    }
+});
 
 
 
+  
