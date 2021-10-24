9000a9d2088119fbfcf3d4333be08ffc99183321
AnmolSinghMahil <63561078+AnmolSinghMahil@users.noreply.github.com>
2021-10-24 11:11:45 -0700

    nav-toggle and nav-close works now
    when pressing any of the nav-links it removes the links from top

--- script/script.js
+++ script/script.js
@@ -1,22 +1,17 @@
+const openModal = document.querySelector(".show-modal");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close");
+const blur1 = document.querySelector(".blur");
 
+openModal.addEventListener("click", function () {
+  modal.classList.remove("hidden");
+  blur1.classList.remove("hidden");
+});
 
-const openModal = document.querySelector('.show-modal');
-const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.close');
-const blur1 = document.querySelector('.blur');
-
-openModal.addEventListener('click',function(){
-    modal.classList.remove('hidden');
-    blur1.classList.remove('hidden');
-})
-
-closeModal.addEventListener('click',function(){
-    modal.classList.add('hidden');
-    blur1.classList.add('hidden');
-})
-
-
-
+closeModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  blur1.classList.add("hidden");
+});
 
 // const homeScroll = document.querySelector('#homelink');
 // const homeSection = document.querySelector('#home-section');
@@ -27,7 +22,6 @@ closeModal.addEventListener('click',function(){
 
 // })
 
-
 // const aboutScroll = document.getElementById('aboutlink');
 // const aboutSection = document.querySelector('#about-section');
 // aboutScroll.addEventListener('click',function(e){
@@ -35,7 +29,6 @@ closeModal.addEventListener('click',function(){
 //     aboutSection.scrollIntoView({behavior:"smooth"});
 //     console.log('about was clicked');
 // })
-
 
 // const portfolioSection = document.querySelector('#portfolio-section');
 // const portfolioScroll = document.querySelector('#Portfolio');
@@ -45,47 +38,49 @@ closeModal.addEventListener('click',function(){
 //     portfolioSection.scrollIntoView({behavior:"smooth"});
 // })
 
- 
-// Event delegation 
+// Event delegation
 // select the commmon element which is '.list'
 // determine where the target event is happening with e.target
-document.querySelector('.list').addEventListener('click', function (e) {
-
-    e.preventDefault();
-    //we only want to select the target with class 'scroll' not something around the link
-    if (e.target.classList.contains('scroll')) {
+document.querySelector(".list").addEventListener("click", function (e) {
+  e.preventDefault();
+  //we only want to select the target with class 'scroll' not something around the link
+  if (e.target.classList.contains("scroll")) {
     // get the href attribute which has the section you want to go to
-        const id = e.target.getAttribute('href');
+    const id = e.target.getAttribute("href");
     // and select e.target/id and use the function scrollIntoView on that id, like section--2
-        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-    }
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
 
-const scrollDown = document.querySelector('.scroll-down');
-const nextSection = document.querySelector('#section--2');
+const scrollDown = document.querySelector(".scroll-down");
+const nextSection = document.querySelector("#section--2");
 
-scrollDown.addEventListener('click',function(){
-    nextSection.scrollIntoView({behavior:"smooth"});
-})
-
+scrollDown.addEventListener("click", function () {
+  nextSection.scrollIntoView({ behavior: "smooth" });
+});
 
 //----------------------Nav-Responsive--------------------//
 
 const closebtn = document.querySelector("#nav-close");
 const togglebtn = document.querySelector("#nav-toggle");
-const navlinks= document.querySelector(".nav-links")
+const navlist = document.querySelector(".list");
 
-closebtn.addEventListener("click", ()=>{
+closebtn.addEventListener("click", () => {
+  navlist.classList.remove("show-menu");
 
-    closebtn.classList.remove('hidden');
-    closebtn.classList.add("show");
-    console.log("closebtn was clicked")
-    
-})
+  console.log("closebtn was clicked");
+});
 
-if(togglebtn) {
-    togglebtn.addEventListener("click",()=>{
-        navlinks.classList.add("show-menu");
-        console.log("toggle is pressed");
-    })
+if (togglebtn) {
+  togglebtn.addEventListener("click", () => {
+    navlist.classList.add("show-menu");
+    console.log("toggle is pressed");
+  });
 }
+
+const navlinks = document.querySelectorAll(".scroll");
+
+function removeMenu() {
+  navlist.classList.remove("show-menu");
+}
+navlinks.forEach((n) => n.addEventListener("click", removeMenu));
